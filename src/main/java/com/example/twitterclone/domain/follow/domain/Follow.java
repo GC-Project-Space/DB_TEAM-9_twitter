4b5953bcbe2fcdd9c340e12ea4371d0dc29aa24a
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2023-11-24 14:29:23 +0900

    :sparkles: [Feat] Follow 관련 Entity 생성 (#7)
    - Follow.java

--- src/main/java/com/example/twitterclone/domain/follow/domain/Follow.java
+++ src/main/java/com/example/twitterclone/domain/follow/domain/Follow.java
@@ -1,5 +1,6 @@
 package com.example.twitterclone.domain.follow.domain;
 
+import com.example.twitterclone.domain.users.domain.Users;
 import com.example.twitterclone.global.common.entity.BaseEntity;
 import com.example.twitterclone.global.constant.Status;
 import lombok.*;
@@ -19,4 +20,11 @@ public class Follow {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JoinColumn(name = "to_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Users to_user; // 팔로우 당하는 사람
+
+    @JoinColumn(name = "from_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Users from_user; // 팔로우 하는 사람
 }
