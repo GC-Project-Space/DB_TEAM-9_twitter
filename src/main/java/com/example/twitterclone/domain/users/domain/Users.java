e9ba71371050d86e0f620f7b467f3b3e3cf2f7c3
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2023-11-24 14:29:50 +0900

    :sparkles: [Feat] Users 관련 Entity 생성 (#7)
    - Users.java
    - Gender.java

--- src/main/java/com/example/twitterclone/domain/users/domain/Users.java
+++ src/main/java/com/example/twitterclone/domain/users/domain/Users.java
@@ -1,11 +1,13 @@
 package com.example.twitterclone.domain.users.domain;
 
+import com.example.twitterclone.domain.users.util.Gender;
 import com.example.twitterclone.global.common.entity.BaseEntity;
 import com.example.twitterclone.global.constant.Status;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 
 @Slf4j
 @Getter
@@ -18,6 +20,31 @@ public class Users extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "email", nullable = false, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "nickname", nullable = false, length = 100)
+    private String nickname;
+
+    @Column(name = "phone", nullable = false, length = 100)
+    private String phone;
+
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender; // 성별: MALE, FEMALE, SECRET
+
+    @Column(name = "brith", nullable = false, length = 100)
+    private String brith;
+
+    @Column(name = "profile_image", length = 100)
+    private String profileImage;
 
     @Column(name = "status", nullable = false)
     @Enumerated(EnumType.STRING)
